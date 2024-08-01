7ab2f034aad11217e657104c4037d9400d3d2fec
Pablo Alan <pabloxt14@gmail.com>
2024-08-01 20:34:53 -0300

    chore: prisma orm fundamentals

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 export const app = fastify()
+
+prisma.user.create({
+  data: {
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+  },
+})
