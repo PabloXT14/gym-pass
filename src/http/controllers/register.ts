c76c9c3b089b9efc189569988b590321de96d5be
Pablo Alan <pabloxt14@gmail.com>
2024-08-18 20:00:00 -0300

    feat: handling use case errors

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 import { RegisterUseCase } from '@/use-cases/register-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import z from 'zod'
@@ -19,11 +20,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     await registerUseCase.execute({ name, email, password })
   } catch (error) {
-    if (error instanceof Error) {
-      return reply.status(400).send({ message: error.message })
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
     }
 
-    return reply.status(500).send()
+    return reply.status(500).send() // TODO: fix me
   }
 
   return reply.status(201).send()
