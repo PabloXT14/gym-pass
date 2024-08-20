4ecfac9a1d85ab3d702470b8fbfb1d13a581453a
Pablo Alan <pabloxt14@gmail.com>
2024-08-20 20:00:00 -0300

    feat: create first unit test

--- src/use-cases/register-use-case.spec.ts
+++ src/use-cases/register-use-case.spec.ts
@@ -1,5 +1,35 @@
-import { expect, test } from 'vitest'
+import { describe, expect, it } from 'vitest'
+import { RegisterUseCase } from './register-use-case'
+import { compare } from 'bcryptjs'
 
-test('should be able to register a use case', () => {
-  expect(2 + 2).toBe(4)
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail(email) {
+        return null
+      },
+      async create(data) {
+        return {
+          id: 'user-01',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
 })
