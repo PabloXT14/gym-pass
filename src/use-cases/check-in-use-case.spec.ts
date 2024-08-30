a70be48ece41a57457035f7f0735b8be1d6ba775
Pablo Alan <pabloxt14@gmail.com>
2024-08-30 20:54:05 -0300

    feat: create check-in use case

---
+++ src/use-cases/check-in-use-case.spec.ts
@@ -0,0 +1,22 @@
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { CheckInUseCase } from './check-in-use-case'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let checkInUseCase: CheckInUseCase
+
+describe('Check In Use Case', () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    checkInUseCase = new CheckInUseCase(checkInsRepository)
+  })
+
+  it('should be able to check in', async () => {
+    const { checkIn } = await checkInUseCase.execute({
+      userId: 'user-01',
+      gymId: 'gym-01',
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+})
