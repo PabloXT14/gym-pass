31853139326658946ba9d3963f0a77b1a030a333
Pablo Alan <pabloxt14@gmail.com>
2024-09-05 18:14:45 -0300

    feat: create fetch user check-ins history use case

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -39,4 +39,12 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
     return checkInOnSameDate
   }
+
+  async findManyByUserId(userId: string, page: number) {
+    const checkIns = this.items
+      .filter((checkIn) => checkIn.user_id === userId)
+      .slice((page - 1) * 20, page * 20) // 20 items per page
+
+    return checkIns
+  }
 }
