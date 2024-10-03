8d0e14281ff2871c1a6709d8f5001c598fab6111
Pablo Alan <pabloxt14@gmail.com>
2024-10-03 20:00:00 -0300

    feat: create other gym controllers

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,9 +1,14 @@
 import { FastifyInstance } from 'fastify'
 import { create } from './create'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
+import { nearby } from './nearby'
+import { search } from './search'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt) // todas as rotas daqui precisaram de autenticação por token
 
   app.post('/gyms', create)
+
+  app.get('/gyms/nearby', nearby)
+  app.get('/gyms/search', search)
 }
