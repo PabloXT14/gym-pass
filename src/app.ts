e73bb481425dd4aafcf520729fa5dfee0880ffa3
Pablo Alan <pabloxt14@gmail.com>
2024-10-07 20:59:42 -0300

    feat: refresh token strategy

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,26 @@
 import fastify from 'fastify'
 import { ZodError } from 'zod'
 import { env } from './env'
-import fastifyJwt from '@fastify/jwt'
 import { usersRoutes } from './http/controllers/users/routes'
 import { gymsRoutes } from './http/controllers/gyms/routes'
 import { checkInsRoutes } from './http/controllers/check-ins/routes'
+import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 
 export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m', // 10 minutes (para validar o token de autenticação novamente)
+  },
 })
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -31,9 +31,27 @@ export async function authenticate(
       },
     )
 
-    return reply.status(200).send({
-      token,
-    })
+    const refreshToken = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: '7d',
+        },
+      },
+    )
+
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/', // rotas da aplicação que podem ter acesso ao cookie (/ = todas as rotas)
+        secure: true, // somente HTTPS pode ter acesso ao cookie
+        sameSite: true, // se o cookie vai ser acessível somente pelo mesmo site ou domínio
+        httpOnly: true, // somente o servidor da aplicação pode ter acesso ao cookie (e nao pelo front-end ou browser)
+      })
+      .status(200)
+      .send({
+        token,
+      })
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message })
