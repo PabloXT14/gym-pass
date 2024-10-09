fbafe823fc51e71146d9656844dc0ca0f6a517d6
Pablo Alan <pabloxt14@gmail.com>
2024-10-09 20:56:25 -0300

    feat: implemented RBAC

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -3,11 +3,12 @@ import { create } from './create'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { nearby } from './nearby'
 import { search } from './search'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt) // todas as rotas daqui precisaram de autenticação por token
 
-  app.post('/gyms', create)
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create)
 
   app.get('/gyms/nearby', nearby)
   app.get('/gyms/search', search)
